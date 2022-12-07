c5647c8b1d0b53615c1ef174479d713ba4ef88b4
caujasutom <nadruk@seznam.cz>
2022-12-07 22:25:43 +0100

    Auto-generated commit [ci skip]

--- assets/Scripts/main.js
+++ assets/Scripts/main.js
@@ -1,7 +1,9 @@
 window.onscroll = () => {
-    if (window.scrollY > 150) {
-        document.querySelector('nav.navbar').classList.add('background');
-    } else {
-        document.querySelector('nav.navbar').classList.remove('background');
-    }
+  if (window.scrollY > 150) {
+    document.querySelector("nav.navbar").classList.remove("no-background");
+    document.querySelector("nav.navbar").classList.add("background");
+  } else {
+    document.querySelector("nav.navbar").classList.remove("background");
+    document.querySelector("nav.navbar").classList.add("no-background");
+  }
 };
