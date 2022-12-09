eec3e9a56563416109007f93d2abf219d4f9f09f
caujasutom <85121888+caujasutom@users.noreply.github.com>
2022-12-09 16:25:00 +0100

    Delete assets directory

--- assets/Scripts/main.js
+++
@@ -1,9 +0,0 @@
-window.onscroll = () => {
-  if (window.scrollY > 150) {
-    document.querySelector("nav.navbar").classList.remove("no-background");
-    document.querySelector("nav.navbar").classList.add("background");
-  } else {
-    document.querySelector("nav.navbar").classList.remove("background");
-    document.querySelector("nav.navbar").classList.add("no-background");
-  }
-};
